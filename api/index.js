7470514f26b288eedd0b8c4529f80ec8280075c5
root <root@Wilson-Desktop.localdomain>
2022-10-27 14:21:08 -0700

    juicebox part 2 2/3 we are on create users

--- api/index.js
+++ api/index.js
@@ -38,6 +38,14 @@ apiRouter.use(async (req, res, next) => {
   }
 });
 
+apiRouter.use((req, res, next) => {
+  if (req.user) {
+    console.log("User is set:", req.user);
+  }
+
+  next();
+});
+
 apiRouter.use((error, req, res, next) => {
   res.send({
     name: error.name,
